995fa6feb78a5ff6774465c8ae86c7691bd53163
DM <dionemorales@outlook.com>
2017-12-05 21:42:23 +1100

    Solutions for Day 5 part 1,2

---
+++ Day-5/main.py
@@ -0,0 +1,20 @@
+
+def execute_instruction(instructions):
+
+    steps = 0
+    curIndex = 0
+    while curIndex < len(instructions) and curIndex >= 0:
+        curInstr = int(instructions[curIndex]) #gets the value of the instr poitned by index
+        if curInstr >= 3:
+            instructions[curIndex] -= 1
+        else:
+            instructions[curIndex] += 1 #remove conditional and keep this line for part 1
+        curIndex += curInstr #update where the index is pointing to
+        steps += 1
+    return steps
+
+if __name__ == "__main__":
+    with open("input.txt") as f:
+        inputString = f.read().splitlines()
+    steps_needed = execute_instruction(list(map(int, inputString)))
+    print("steps needed = " + str(steps_needed))
