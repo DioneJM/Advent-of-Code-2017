58456da46f3e6ddb4d0e4878db4062305cfa2ea2
DM <dionemorales@outlook.com>
2017-12-10 22:25:34 +1100

    Solutions for Day 10 Parts 1,2

---
+++ Day-10/main.py
@@ -0,0 +1,53 @@
+from itertools import cycle
+
+
+def knotHash(size, lengths, iterations):
+    inpList = list(range(size))
+    listLen = len(inpList)
+    product = 0
+    skipSize = 0
+    curInd = 0
+    move = 0
+    for i in range(iterations):  
+        for length in lengths:
+            index = curInd
+            subList = [None]*length
+            for i in range(length):
+                subList[i] = inpList[(index+i)%len(inpList)]
+            offset = 0
+            for num in reversed(subList):
+                inpList[(index+offset)%len(inpList)] = num
+                offset += 1
+            move = length + skipSize
+            curInd += move
+            skipSize += 1
+
+    product = inpList[0]*inpList[1]
+
+    denseHash = str()
+    for block in range(listLen//16):
+        denseVal =  0
+        for sparse in range(16):
+            denseVal ^= inpList[block * 16 + sparse]
+        denseHash += hex(denseVal)[2:].zfill(2)
+
+    print("Product:", product)
+    print("Knot Hash:", denseHash)
+    print("Iterations:", iterations)
+if __name__ == "__main__":
+    maxVal = 256#256 for puzzle
+    fileIn = open("input.txt").read()
+    hashKey1 = [int(i) for i in fileIn.strip().split(",")]
+    hashKey2 = [ord(x) for x in fileIn.strip()] + [17, 31, 73, 47, 23]
+    testHash = [ord(c) for c in "AoC 2017"] +  [17, 31, 73, 47, 23]
+
+    print("-----")
+    print("Running for part 1...")
+    knotHash(maxVal, hashKey1, 1)
+    print("-----")
+    print("Running for part 2...")
+    knotHash(maxVal, hashKey2, 64)
+    print("-----")
+    
+
+
